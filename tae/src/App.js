6aceda28c783cc752a4c1f535c5d2fce97c35618
Harshita choudhary <harshitachoudhary130@gmail.com>
2025-05-12 12:55:48 +0000

    commited

---
+++ tae/src/App.js
@@ -0,0 +1,13 @@
+import ProductDetail from "./ProductDetails/ProductDetail";
+import './ProductDetails/product.css'
+import './ProductDetails/Component/SimilarItem/similar.css'
+
+function App() {
+  return (
+   <>
+   <ProductDetail/>
+   </>
+  );
+}
+
+export default App;
